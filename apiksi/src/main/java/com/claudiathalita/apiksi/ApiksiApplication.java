c60bbe8ad55a12e91ae3bd06b6c3431f39c31067
ClaudiaThalita <claudiathalitamendes@outlook.com>
2023-05-05 20:06:18 -0300

    feature: criação do projeto

---
+++ apiksi/src/main/java/com/claudiathalita/apiksi/ApiksiApplication.java
@@ -0,0 +1,13 @@
+package com.claudiathalita.apiksi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiksiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiksiApplication.class, args);
+	}
+
+}
---
+++ apiksi/src/test/java/com/claudiathalita/apiksi/ApiksiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudiathalita.apiksi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiksiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

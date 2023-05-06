135d023770cdeacb914bcae9caa0f3bcf2a9f1d8
ClaudiaThalita <claudiathalitamendes@outlook.com>
2023-05-05 21:15:27 -0300

    adjust: Ajuste da classe Cliente, criação do construtor, getter, setters e hashcode equals

--- apiksi/src/main/java/com/claudiathalita/apiksi/Cliente.java
+++ apiksi/src/main/java/com/claudiathalita/apiksi/Cliente.java
@@ -1,5 +1,8 @@
 package com.claudiathalita.apiksi;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,14 +13,67 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
-public class Cliente {
+public class Cliente implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
 	private String email;
+	
+	public Cliente() {
+		
+	}
+
+	public Cliente(Integer id, String nome, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
 	
 	
 	
